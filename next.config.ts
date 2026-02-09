76059ea85c43e2f849fd5468acf0e0f25b89d043
domfam-geonwoong <geonwoong.jeong@domfam.com>
2026-02-09 14:43:10 +0900

    chore: add confix property (img opt)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export', // 정적 사이트 생성
+  output: 'export', 
+  images: {
+    unoptimized: true,
+  },
   basePath: '/character-maker', // GitHub Pages 서브패스
 };
 
